7c2a4aefd13538e3aa727dda953c18ba3ce8e648
Parth200611 <parthdahapute7@gmail.com>
2025-03-11 12:49:41 +0530

    commit

---
+++ app/src/main/java/com/mountreachsolution/vibze/AboutUs.java
@@ -0,0 +1,23 @@
+package com.mountreachsolution.vibze;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_about_us);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.lavender));
+        getWindow().setNavigationBarColor(ContextCompat.getColor(this,R.color.white));
+
+    }
+}
--- app/src/main/java/com/mountreachsolution/vibze/User/UserHomepage.java
+++ app/src/main/java/com/mountreachsolution/vibze/User/UserHomepage.java
@@ -1,26 +1,94 @@
 package com.mountreachsolution.vibze.User;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowInsetsController;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.mountreachsolution.vibze.AboutUs;
 import com.mountreachsolution.vibze.R;
 
-public class UserHomepage extends AppCompatActivity {
-
+public class UserHomepage extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_homepage);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.lavender)));
+        Window window = getWindow();
+        window.setStatusBarColor(getResources().getColor(R.color.lavender));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false);
+            window.getInsetsController().setSystemBarsAppearance(0, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+        }
+
+        getWindow().setNavigationBarColor(ContextCompat.getColor(UserHomepage.this,R.color.white));
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        bottomNavigationView = findViewById(R.id.bottomnevigatiomuserhome);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        bottomNavigationView.setSelectedItemId(R.id.UserHome);
+
+
+
+    }
+    UserHome userHome = new UserHome();
+    UserStatus userRequest=new UserStatus();
+    UserProfil userProfil = new UserProfil();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.UserHome) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayoutuserhome, userHome).commit();
+        } else if (item.getItemId() == R.id.status) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayoutuserhome, userRequest).commit();
+        } else if (item.getItemId() == R.id.UserProfil) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayoutuserhome, userProfil).commit();
+
+        }
+        return true;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menuuser,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.Request){
+            Intent i = new Intent(UserHomepage.this, RequestPage.class);
+            startActivity(i);
+
+        }else if (item.getItemId()==R.id.Aboutus){
+            Intent i = new Intent(UserHomepage.this, AboutUs.class);
+            startActivity(i);
+
+        }else if (item.getItemId()==R.id.AddUser){
+            Intent i = new Intent(UserHomepage.this, AddUser.class);
+            startActivity(i);
+
+        }
+
+        return true;
     }
 }
